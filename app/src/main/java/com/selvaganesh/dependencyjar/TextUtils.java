0b9b4a6585b926a68d38166ef12379aec12142fc
Selva Ganesh M <selva.ganesh@bofin.com>
2019-08-21 12:42:06 +0530

    first commit

---
+++ app/src/main/java/com/selvaganesh/dependencyjar/TextUtils.java
@@ -0,0 +1,124 @@
+package com.selvaganesh.dependencyjar;
+
+import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+
+import androidx.core.content.ContextCompat;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TextUtils {
+
+    public static boolean isEmpty(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    public static String getLocalString(Object input) {
+        return input.toString().trim();
+    }
+
+    public static int getInt(Object input) {
+        try {
+            return input == null ? 0 : (int) input;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public static long getLong(Object input) {
+        try {
+            return input == null ? 0 : (long) input;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public static double getDouble(Double value) {
+        return value == null ? 0.0 : value;
+    }
+
+    public static String getString(Object input) {
+        return input == null ? "" : input.toString().trim().equals("") ? "" :
+                input.toString().trim().equalsIgnoreCase("null") ? "" : input.toString().trim();
+    }
+
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(.\\d+)?");
+    }
+
+    public static String getFirstChar(String sss) {
+        String returnString = "";
+        if (!TextUtils.isEmpty(sss)) {
+            try {
+                /*Pattern p = Pattern.compile("\\b[a-zA-Z]");
+                Matcher m = p.matcher(sss);
+
+                while (m.find()) {
+                    returnString += m.group();
+                }*/
+
+                for (String s : sss.split(" ")) {
+                    returnString += s.charAt(0);
+                }
+
+
+                if (returnString.equals("+")) {
+                    returnString = sss.substring(1, 2);
+                }
+
+                if (returnString.length() > 2) {
+                    returnString = returnString.substring(0, 2);
+                }
+
+                if (TextUtils.isEmpty(returnString)) {
+                    returnString = "Z";
+                }
+
+            } catch (Exception e) {
+                UiUtils.appErrorLog("TextUtils", e.getMessage());
+            }
+        }
+        return getString(returnString).toUpperCase();
+
+    }
+
+
+    public static SpannableStringBuilder getColorText(Context context, String filterString, String actualString, int textColor) {
+
+
+        SpannableStringBuilder sb = new SpannableStringBuilder(TextUtils.getString(actualString));
+        try {
+            Pattern word = Pattern.compile(TextUtils.getString(filterString).toLowerCase());
+            Matcher match = word.matcher(actualString.toLowerCase());
+            while (match.find()) {
+                ForegroundColorSpan fcs = new ForegroundColorSpan(
+                        ContextCompat.getColor(context, textColor)); //specify color here
+                sb.setSpan(fcs, match.start(), match.end(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            }
+        } catch (Exception e) {
+            UiUtils.appErrorLog("TextUtils", e.getMessage());
+        }
+        return sb;
+
+    }
+
+    /**
+     * Returns whether the given CharSequence contains only digits.
+     */
+    public static boolean isDigitsOnly(CharSequence str) {
+        final int len = str.length();
+        for (int cp, i = 0; i < len; i += Character.charCount(cp)) {
+            cp = Character.codePointAt(str, i);
+            if (!Character.isDigit(cp)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
---
+++ app/src/main/java/com/selvaganesh/dependencyjar/UiUtils.java
@@ -0,0 +1,163 @@
+package com.selvaganesh.dependencyjar;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.text.Html;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import java.text.DecimalFormat;
+
+
+
+public class UiUtils {
+
+    public static final int NUM_HASHED_BYTES = 9;
+    public static final int NUM_BASE64_CHAR = 11;
+    //SMS
+    private static final String HASH_TYPE = "SHA-256";
+    public static Dialog clDialog, connectBankDia;
+    public static TextView clMessage_txt, tv_connect_msg, tv_cancel;
+    private static String TAG = UiUtils.class.getSimpleName();
+    private static Toast mToast;
+    //timer
+    private static CountDownTimer timer = null;
+    private static boolean pauseTimer = false;
+    private static boolean timerRunning = false;
+    private static int expireIn = 60;
+
+    public static void showToast(Context context, String input) {
+        try {
+            if (!TextUtils.isEmpty(input)) {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    mToast = Toast.makeText(context, input, Toast.LENGTH_SHORT);
+                    mToast.setGravity(Gravity.CENTER, 0, 0);
+                    mToast.show();
+                } else {
+                    mToast = Toast.makeText(context, input, Toast.LENGTH_SHORT);
+                    mToast.setGravity(Gravity.CENTER, 0, 0);
+                    CountDownTimer toastCountDown;
+                    int toastDurationInMilliSeconds = 1000;
+                    toastCountDown = new CountDownTimer(toastDurationInMilliSeconds, 100) { /*Tick duration*/
+                        public void onTick(long millisUntilFinished) {
+                            mToast.show();
+                        }
+
+                        public void onFinish() {
+                            mToast.cancel();
+                        }
+                    };
+                    mToast.show();
+                    toastCountDown.start();
+                }
+            }
+        } catch (Exception e) {
+            UiUtils.appErrorLog(TAG, "Toast Ex : " + e.getMessage());
+        }
+    }
+
+    public static void appLog(String tag, String msg) {
+        try {
+            Log.v(tag, msg);
+        } catch (Exception e) {
+            //Crashlytics.logException(e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void appErrorLog(String tag, String msg) {
+        try {
+            Log.e(tag, msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static double getDoubleAmount(String s) {
+        s = s.replaceAll(",", "");
+        DecimalFormat decimalFormat = new DecimalFormat("##0.00");
+        decimalFormat.setMaximumFractionDigits(2);
+        Double returnAmount = 0.00;
+        if (!TextUtils.isEmpty(s)) {
+            try {
+                returnAmount = Double.parseDouble(decimalFormat.format(Double.valueOf(s)));
+            } catch (Exception e) {
+                UiUtils.appErrorLog("getDoubleAmount", e.getMessage());
+            }
+        }
+        return returnAmount;
+    }
+
+    public static double getAmount(Double s) {
+        DecimalFormat decimalFormat = new DecimalFormat("##0.00");
+        decimalFormat.setMaximumFractionDigits(2);
+        Double returnAmount = 0.00;
+        if (s != null) {
+            try {
+                returnAmount = Double.parseDouble(decimalFormat.format(s));
+            } catch (Exception e) {
+                UiUtils.appErrorLog("getToAccCurencyAmount", e.getMessage());
+            }
+        }
+        return returnAmount;
+    }
+
+    public static int getIntFromString(String s) {
+        int i = 0;
+        try {
+            i = Integer.parseInt(s);
+        } catch (Exception e) {
+            UiUtils.appErrorLog("getIntParse", e.getMessage());
+        }
+        return i;
+    }
+
+    public static Drawable getDrawable(Resources resources, int id) {
+        return ResourcesCompat.getDrawable(resources, id, null);
+    }
+
+    public static int getColor(Resources resources, int id) {
+        return ResourcesCompat.getColor(resources, id, null);
+    }
+
+    public static String removeDot(String txt) {
+        String inputValue = TextUtils.getString(txt);
+        String returnValue = inputValue;
+        try {
+            if (!TextUtils.isEmpty(inputValue) && inputValue.contains(".")) {
+                String[] separated = inputValue.split("\\.");
+                String prefix = separated[0];
+                String suffix = TextUtils.getString(separated[1]);
+                if (suffix.length() == 0 || (suffix.equals("0") || suffix.equals("00"))) {
+                    returnValue = prefix;
+                } else {
+                    returnValue = inputValue;
+                }
+            } else {
+
+                returnValue = inputValue;
+            }
+        } catch (Exception e) {
+            UiUtils.appErrorLog("removeDot", e.getMessage());
+        }
+
+        return returnValue;
+    }
+
+    public static void setHtmlString(TextView textView, String msg) {
+        String message = TextUtils.getString(msg);
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            textView.setText(Html.fromHtml(message));
+        } else {
+            textView.setText(Html.fromHtml(message, Html.FROM_HTML_MODE_COMPACT));
+        }
+    }
+}
